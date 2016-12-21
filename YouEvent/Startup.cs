a7a0e99bca706ee129e02fa9a577938225924dd7
adratwa <dratwa.adam@gmail.com>
2016-12-21 18:06:11 +0100

    adding project

---
+++ YouEvent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YouEvent.Startup))]
+namespace YouEvent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
