db1b965d997e95a69374e4d33a0216c3e4280fd0
adamdratwa <adam.dratwa@fabres.pl>
2016-12-22 13:10:26 +0100

    stylizying material-ui

--- YouEvent/Scripts/init.jsx
+++ YouEvent/Scripts/init.jsx
@@ -1,12 +1,13 @@
 ﻿import * as React from 'react';
 import * as ReactDOM from 'react-dom'
 import getMuiTheme from "material-ui/styles/getMuiTheme";
+import {cyan500} from 'material-ui/styles/colors';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import TableExampleSimple from "./index.jsx";
 
 const muiTheme = getMuiTheme({
     palette: {
-        primary1Color: "#476471"
+        primary1Color: cyan500
     }
 });
 
